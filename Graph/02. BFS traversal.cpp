d18ce3d3ab7985ac8464f81374183f498f17f5c1
Rahul <50038276+itsrahulhere@users.noreply.github.com>
2022-02-24 21:27:05 +0530

    added 02

--- Graph/02. BFS traversal.cpp
+++ Graph/02. BFS traversal.cpp
@@ -1,11 +1,11 @@
-
-
-    vector<int>bfsOfGraph(int V, vector<int> adj[])
+// #version 1 - Source Given & Graph is Connected
+//Problem: 
+    vector<int>bfsOfGraph(int V, vector<int> adj[]) //source given int the parameter
     {
 	    vector<int> bfs; 
-	    vector<int> vis(V, 0); 
+	    vector<int> vis(V, 0); // 0-based indexing
 	    queue<int> q; 
-	    q.push(0); 
+	    q.push(0); //taking source=0
 	    vis[0] = 1; 
 	    while(!q.empty())
         {
@@ -13,7 +13,7 @@
 	        q.pop(); 
 	        bfs.push_back(node); 
 	        
-	        for(auto it : adj[node]) {
+	        for(auto it : adj[node]) { //here auto = int
 	            if(!vis[it]) {
 	                q.push(it); 
 	                vis[it] = 1; 
@@ -21,5 +21,33 @@
 	        }
 	    }
 	    
+	    return bfs; 
+	}
+
+// #version 2 - No Source Given and Graph may be disconnected
+	vector<int>bfsOfGraph(int V, vector<int> adj[])
+    {
+	    vector<int> bfs; 
+	    vector<int> vis(V+1, 0); // 1-based indexing
+	    
+		for(int i=1; i<=V; i++){
+			if(!vis[i]){
+				queue<int> q; 
+				q.push(0); 
+				vis[0] = 1; 
+				while(!q.empty()){
+					int node = q.front();
+					q.pop(); 
+					bfs.push_back(node); 
+					
+					for(auto it : adj[node]) {
+						if(!vis[it]) {
+							q.push(it); 
+							vis[it] = 1; 
+						}
+					}
+				}
+			}
+		}
 	    return bfs; 
 	}
